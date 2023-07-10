6ef9f239d1e157e523876f319d532f46411357eb
kural <1398353676@qq.com>
2023-07-10 20:35:47 +0800

    pypypypypy

--- src/riscv_gen/live_interval.rs
+++ src/riscv_gen/live_interval.rs
@@ -4,47 +4,54 @@ use crate::structures::riscv_struct::*;
 use crate::riscv_gen::reg::*;
 
 impl AsmFunc {
-    // 将该块的后继块放入栈
-    fn push_sux_in_stack(&self, stk: &Vec<(usize, Vec<&String>, usize)>, block: &AsmBlock) {
-        let next_block = block.sux.iter().collect::<Vec<_>>();
-        if !next_block.is_empty() {
-            stk.push((0, next_block, 0));
-        }
-    }
-
     // 深度优先编号
     fn dfs(&self, block_idx: &BTreeMap<&str, usize>) -> Vec<usize> {
-        let mut res = vec!();
-        // vec<pre_id, now_id, has visited>
-        let mut stk: Vec<(usize, Vec<&String>, usize)> = vec!();
-        // block_state表示该块已搜索
+        // 访问序列结果
+        let mut res = Vec::new();
+        // 访问栈<pre, successor, has visited>
+        let mut stk:Vec<(usize, Vec<&String>, usize)> = Vec::new();
+        // 已访问状态
         let mut visited: BTreeSet<usize> = BTreeSet::new();
-
-        // 搜索起始块，然后存入其后继块
+        
+        // 访问初始块
         visited.insert(0);
         let block = self.blocks.get(0).unwrap();
         res.push(0);
         
-        self.push_sux_in_stack(&stk, block);
-
+        // 放入其后续块
+        let next_block:Vec<&String> = block.successor.iter().collect::<Vec<_>>();
+        if !next_block.is_empty() {
+            stk.push((0, next_block, 0));
+        }
+    
         // 深度优先搜索
-        while !stk.is_empty() {
-            let item = stk.pop().unwrap();
+        while let Some(item) = stk.pop() {
+            // 获得当前块
             let block_id = block_idx.get(item.1[item.2].as_str()).unwrap();
-
-            // 该块栈中还有块没有遍历，放回去，计数指向下一个块
+            
+            // 当前块pre的后续块如果没有访问完，重新入栈
             if item.2 < item.1.len() - 1 {
                 stk.push((item.0, item.1, item.2 + 1));
             }
-
-            // 如果该块没有遍历，将该块加入遍历顺序结果，加入该块的后续块
-            if visited.contains(block_id) == false {
+            
+            // 如果未访问，放入其后续块
+            if !visited.contains(block_id) {
                 visited.insert(*block_id);
                 res.push(*block_id);
                 let block = self.blocks.get(*block_id).unwrap();
-
+                let next_block:Vec<&String> = block.successor.iter().collect::<Vec<_>>();
+                if !next_block.is_empty() {
+                    stk.push((0, next_block, 0));
+                }
             }
         }
         res
     }
+
+    pub fn update_order(&mut self, id_to_pos: &BTreeMap<usize, usize>) {
+        for (idx, depth_first_order, _) in self.call_info.iter_mut() {
+            *depth_first_order = Some(*id_to_pos.get(idx).unwrap())
+        }
+    }
 }
+
