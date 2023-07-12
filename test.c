2bfee4821cf31f213feda18b368d90f53a3ac044
郭睆 <gh2002@mail.nwpu.edu.en>
2023-07-12 12:02:16 +0800

    merge test.c

--- test.c
+++ test.c
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-float add(float a, float b){
-    float x=0.5;
-    return a+b+x;
-}
-
-int A[3][2][5];
-int a = 0;
-int b = 10000;
-float c =1.0;
-float d =0.0;
 int main(){
     int arr[4]={1,2};
-    float f1 = 0x1.921fb6p+1;
-    float f2 = 2.+0.4;
-    float f = 1 + 1.0 ;
-    A[2][1][1+1] = 188 + A[1][2][1+1];
-=======
-int main(){
-    int arr[4]={1,2};
->>>>>>> 1ab98900194049ccb039c09448d12375c6b4cc25
     return 0;
 }
