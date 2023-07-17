7cf233984c148ecfdc5faa1e8a59a5a0c161b72f
sukiiiKotori <2020303075@mail.nwpu.edu.cn>
2023-07-17 18:22:37 +0800

    fix

--- tests/test-koopa-riscv.sh
+++
@@ -1,7 +0,0 @@
-ext=".c"
-set_dir="./tests/koopa/"
-dst_dir="./target/koopa/"
-mode="riscv"
-
-./tests/test-base.sh $ext $set_dir $dst_dir $mode $1 $2
-
--- tests/test-llvm.sh
+++
@@ -1,7 +0,0 @@
-ext=".sy"
-set_dir="./tests/compiler2022/"
-dst_dir="./target/compiler2022/"
-mode="llvm"
-
-bash ./tests/test-base.sh $ext $set_dir $dst_dir $mode $1 $2
-
