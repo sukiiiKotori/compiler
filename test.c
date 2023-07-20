d9da4b15d862bc3478d5c086de2812996db73cad
pyy_fyy <1103268336@qq.com>
2023-07-20 18:22:44 +0800

    测试移除rodata段

--- test.c
+++ test.c
@@ -7,5 +7,10 @@ float b = 2.5;
 int main(){
     int a = A[3];
     float farr[5]= {0.1,0.2,0.3,0.4,0.5};
+    putfloat(farr[0]);
+    putfloat(farr[1]);
+    putfloat(farr[2]);
+    putfloat(farr[3]);
+    putfloat(farr[4]);
     return 0;
 }
