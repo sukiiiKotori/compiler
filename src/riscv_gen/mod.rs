201a5bd414c7551468e08a98258ed4bdac000e2b
pyy_fyy <1103268336@qq.com>
2023-07-25 18:43:10 +0800

    加了点注释

--- src/riscv_gen/mod.rs
+++ src/riscv_gen/mod.rs
@@ -22,13 +22,19 @@ use crate::structures::riscv_struct::*;
 
 pub fn generate_asm(program: &LLVMProgram) -> RiscV {
     let mut asm = RiscV::new();
+    //使用LLVM IR来进行数据段的构造，代码段的构造以及指令选择
     program.push_datasection(&mut asm);
     program.push_textsection(&mut asm);
     program.asm_select(&mut asm);
+    //进行寄存器分配
     asm.alloc_regs::<LinearScan>();
+    //在函数的入口保存使用过的s0-s11寄存器
     asm.save_registers();
+    //在函数出口恢复使用过的s0-s11寄存器
     asm.restore_registers();
+    //确定程序栈大小
     asm.deterministic_stack();
+    //分配和释放栈空间
     asm.stack_alloc_free();
     asm.map_stack_address();
     asm
