48ed7b3bc2d77f10faf8edffa77a73d01df26ae0
琴梨梨 <11704182+pyy_fyy@user.noreply.gitee.com>
2023-08-09 21:15:23 +0800

    test.c

--- test.c
+++ test.c
@@ -1,5 +1,25 @@
+// test while-if
+int whileIf() {
+  int a;
+  a = 0;
+  int b;
+  b = 0;
+  while (a < 100) {
+    if (a == 5) {
+      b = 25;
+    }
+    else if (a == 10) {
+      b = 42;  
+    }
+    else {
+      b = a * 2;
+    }
+    a = a + 1;
+  }
+  return (b);
+}
+
 
 int main(){
-    int A[5][4][6] = {0};
-    return A[2][2][1];
+  return (whileIf());
 }
